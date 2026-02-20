82750a6cef1ec7dc433c96232b43f935a38ab117
dfn-slxxp <waldman.sebastian@gmail.com>
2026-02-19 22:51:12 -0500

    Import Wildcats, Remove Lynk

---
+++ Assets/Prefabs/Reefscape/Robots/Mods/Wildcats Mod/9483/WildcatsSetpoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Prefabs.Reefscape.Robots.Mods.Wildcats._9483
+{
+    [CreateAssetMenu(fileName = "Setpoint", menuName = "Robot/Wildcats Setpoint", order = 0)]
+    public class WildcatsSetpoint : ScriptableObject
+    {
+        [Tooltip("Inches")] public float elevatorHeight;
+    }
+}
